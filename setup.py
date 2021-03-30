ad15cfe144156fadb0497d31691a5530033e9007
deepgreenAN <asami73dgreen63@gmail.com>
2021-03-31 02:55:39 +0900

    ディレクトリ構造を変更

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+install_requires = [
+]
+
+packages = [
+    "py_restart"
+]
+
+setup(
+    name='py_restart',
+    version='0.0.0',
+    packages=packages,
+    install_requires=install_requires,
+)
